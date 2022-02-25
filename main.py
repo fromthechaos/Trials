9f8af68cbf0cad0ec915aaaa33f9b726811c5097
wolfxdr <95113963+wolfxdr@users.noreply.github.com>
2022-02-25 11:51:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from creds import name
-from creds import passwd
+from creds import passwd, link1
 import time
 
 signin = (By.XPATH, '//*[@id="office-HeroPhotographic-6j70rzy"]/div/div[3]/section/div/div[2]/div[1]/a')
@@ -28,6 +28,6 @@ time.sleep(4)
 browser.find_element(by=By.XPATH, value='//*[@id="idBtn_Back"]').click()
 time.sleep(10)
 browser.get('https://teams.microsoft.com/_?culture=en-in&country=IN&lm=deeplink&lmsrc=homePageWeb&cmpid=WebSignIn#/calendarv2')
-browser.get('https://teams.microsoft.com/_?culture=en-in&country=IN&lm=deeplink&lmsrc=homePageWeb&cmpid=WebSignIn#/pre-join-calling/19:61f0506dc93e44fa9f872a76ef1fbc6a@thread.tacv2')
+browser.get(link1)
 WebDriverWait(browser, 10).until(c.element_to_be_clickable(joinbutton)).click()
 browser.find_element(by=By.XPATH, value='//*[@id="page-content-wrapper"]/div[1]/div/calling-pre-join-screen/div/div/div[2]/div[1]/div[2]/div/div/section/div[1]/div/div/button')
